ad8467e53f86386374b46533cd693b39f9d57e1b
Lukáš Karas <lukas.karas@centrum.cz>
2016-04-25 23:05:12 +0200

    avoid tries to download OSM tiles with zoom level > 19

--- src/OsmTileDownloader.cpp
+++ src/OsmTileDownloader.cpp
@@ -46,7 +46,12 @@ OsmTileDownloader::~OsmTileDownloader() {
 
 void OsmTileDownloader::download(uint32_t zoomLevel, uint32_t x, uint32_t y)
 {
-  // TODO: move this url to resources
+  if (zoomLevel > 19){
+    emit failed(zoomLevel, x, y);
+    return;
+  }
+  
+  // TODO: move tile provider url and its maximum zoom level to resources
   QChar ch('a' + char(serverNumber % ('a' - 'c')));  
   QUrl tileUrl(QString("http://%1.tile.openstreetmap.org/%2/%3/%4.png")
     .arg(ch).arg(zoomLevel).arg(x).arg(y));  
