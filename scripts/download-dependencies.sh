50352d75108ca687d8ced8a1fb5f167b413336e3
Lukáš Karas <lukas.karas@centrum.cz>
2016-05-06 09:10:08 +0200

    use my fork of libosmscout with some changes that are not merged to upstream yet

--- scripts/download-dependencies.sh
+++ scripts/download-dependencies.sh
@@ -5,6 +5,6 @@ cd "`dirname $0`/.."
 mkdir -p dependencies
 cd dependencies
 
-git clone https://github.com/Framstag/libosmscout.git libosmscout
+git clone https://github.com/karry-space-with-my-second-forks/libosmscout.git libosmscout
 wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/marisa-trie/marisa-0.2.4.tar.gz
 zcat marisa-0.2.4.tar.gz | tar -xf -
