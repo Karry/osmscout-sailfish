b889a3d860be88e85a86d6dbb6c8c46d43a0fbb9
Lukáš Karas <lukas.karas@centrum.cz>
2016-06-23 12:33:29 +0200

    use master branch from my libosmscout fork

--- scripts/download-dependencies.sh
+++ scripts/download-dependencies.sh
@@ -5,7 +5,7 @@ cd "`dirname $0`/.."
 mkdir -p dependencies
 cd dependencies
 
-git clone -b point-sequence https://github.com/karry-space-with-my-second-forks/libosmscout.git libosmscout
+git clone -b master https://github.com/karry-space-with-my-second-forks/libosmscout.git libosmscout
 
 wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/marisa-trie/marisa-0.2.4.tar.gz
 zcat marisa-0.2.4.tar.gz | tar -xf -
