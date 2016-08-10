794585340fa9994b8212ecea9f547915c3ce28af
Lukáš Karas <lukas.karas@centrum.cz>
2016-08-10 10:30:51 +0200

    add czech tranlation

---
+++ translations/cs.ts
@@ -0,0 +1,103 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs_CZ" sourcelanguage="en_US">
+<context>
+    <name>About</name>
+    <message>
+        <source>OSM Scout %1</source>
+        <translation>OSM Scout %1</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>O OSM Scout</translation>
+    </message>
+    <message>
+        <source>OSM Scout for Sailfish OS is developed as open-source by volunteers in their free time. You can help to improve this application by reporting bugs, creating translations or developing new features. Any help is welcome.</source>
+        <translation>OSM Scout pro Sailfish OS je vyvýjen dobrovolníky v jejich volném čase jako open-source. Můžete pomoci vylepšit tuto aplikaci reportováním chyb, vytvářením překladů nebo vývojem nových vlastností. Každá pomoc je výtána.</translation>
+    </message>
+    <message>
+        <source>GitHub page</source>
+        <translation>Stránka na GitHubu</translation>
+    </message>
+    <message>
+        <source>OSMScout library GitHub page</source>
+        <translation>GitHub stránka OSMScout knihovny</translation>
+    </message>
+    <message>
+        <source>Offline map import tutorial</source>
+        <translation>Tutoriál importu offline mapy</translation>
+    </message>
+</context>
+<context>
+    <name>Cover</name>
+    <message>
+        <source>OSM Scout</source>
+        <translation>OSM Scout</translation>
+    </message>
+</context>
+<context>
+    <name>Map</name>
+    <message>
+        <source>Search</source>
+        <translation>Hledat</translation>
+    </message>
+    <message>
+        <source>Where am I?</source>
+        <translation>Kde jsem?</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Nastavení</translation>
+    </message>
+    <message>
+        <source>Layers</source>
+        <translation>Vrstvy</translation>
+    </message>
+    <message>
+        <source>Bookmarks</source>
+        <translation>Oblíbená místa</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>O OSM Scout</translation>
+    </message>
+</context>
+<context>
+    <name>OSMCopyright</name>
+    <message>
+        <source>© OpenStreetMap contributors</source>
+        <translation>© přispěvatelé OpenStreetMap</translation>
+    </message>
+</context>
+<context>
+    <name>PlaceDetail</name>
+    <message>
+        <source>west</source>
+        <translation>západ</translation>
+    </message>
+    <message>
+        <source>east</source>
+        <translation>východ</translation>
+    </message>
+    <message>
+        <source>south</source>
+        <translation>jih</translation>
+    </message>
+    <message>
+        <source>north</source>
+        <translation>sever</translation>
+    </message>
+    <message>
+        <source>You are here</source>
+        <translation>Jste zde</translation>
+    </message>
+    <message>
+        <source>%1 %2 from you</source>
+        <translation>%1 %2 od vás</translation>
+    </message>
+    <message>
+        <source>%1 %2 from</source>
+        <translation>%1 %2 od</translation>
+    </message>
+</context>
+</TS>
