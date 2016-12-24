31d1de4474d0b09094cdc508b34b2f7fa98f7358
Håvard Moen <post@haavard.name>
2016-12-24 12:13:26 +0100

    added norwegian bokmål translation

---
+++ translations/nb.ts
@@ -0,0 +1,219 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nb" sourcelanguage="en">
+<context>
+    <name>About</name>
+    <message>
+        <source>OSM Scout %1</source>
+        <translation>OSM Scout %1</translation>
+    </message>
+    <message>
+        <source>OSM Scout for Sailfish OS is developed as open-source by volunteers in their free time. You can help to improve this application by reporting bugs, creating translations or developing new features. Any help is welcome.</source>
+        <translation>OSM Scout for Sailfish OS er utviklet som fri programvare av frivillige krefter på deres ledige tid. Du kan hjelpe til med å forbedre dette programmet ved å melde inn feil, lage oversettelser eller utvikle ny funksjonalitet. All hjelp er velkommen.</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <source>GitHub page</source>
+        <translation>GitHub side</translation>
+    </message>
+    <message>
+        <source>OSMScout library GitHub page</source>
+        <translation>OSMScout-library GitHub side</translation>
+    </message>
+    <message>
+        <source>Offline map import tutorial</source>
+        <translation>Importeringsguide for offline kart</translation>
+    </message>
+    <message>
+        <source>Translators</source>
+        <translation>Oversettere</translation>
+    </message>
+    <message>
+        <source>Czech: Lukáš Karas</source>
+        <translation>Tjekkisk: Lukáš Karas</translation>
+    </message>
+    <message>
+        <source>Hungarian: Miklós Márton</source>
+        <translation>Ungarsk: Miklós Márton</translation>
+    </message>
+    <message>
+        <source>Polish: Atlochowski</source>
+        <translation>Polsk: Atlochowski</translation>
+    </message>
+    <message>
+        <source>Swedish: Åke Engelbrektson</source>
+        <translation>Svensk: Åke Engelbrektson</translation>
+    </message>
+</context>
+<context>
+    <name>Cover</name>
+    <message>
+        <source>OSM Scout</source>
+        <translation>OSM Scout</translation>
+    </message>
+</context>
+<context>
+    <name>Downloads</name>
+    <message>
+        <source>Download Progress</source>
+        <translation>Nedlastningsfremgang</translation>
+    </message>
+    <message>
+        <source>Download Map</source>
+        <translation>Last ned kart</translation>
+    </message>
+</context>
+<context>
+    <name>Layers</name>
+    <message>
+        <source>Map settings</source>
+        <translation>Kartinstillinger</translation>
+    </message>
+    <message>
+        <source>Map magnification</source>
+        <translation>Kartforstørrelse</translation>
+    </message>
+    <message>
+        <source>Online Maps</source>
+        <translation>Online-kart</translation>
+    </message>
+    <message>
+        <source>Enable online maps</source>
+        <translation>Skru på online-kart</translation>
+    </message>
+    <message>
+        <source>Style</source>
+        <translation>Stil</translation>
+    </message>
+    <message>
+        <source>Offline Maps</source>
+        <translation>Offline-kart</translation>
+    </message>
+    <message>
+        <source>Enable offline map</source>
+        <translation>Skru på offline-kart</translation>
+    </message>
+    <message>
+        <source>Sea rendering</source>
+        <translation>Sjørendering</translation>
+    </message>
+    <message>
+        <source>Sea can overlap online tiles!</source>
+        <translation>Sjø kan overlappe online-ruter</translation>
+    </message>
+</context>
+<context>
+    <name>Map</name>
+    <message>
+        <source>Search</source>
+        <translation>Søk</translation>
+    </message>
+    <message>
+        <source>Where am I?</source>
+        <translation>Hvor er jeg?</translation>
+    </message>
+    <message>
+        <source>Bookmarks</source>
+        <translation>Bokmerker</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <source>Map downloads</source>
+        <translation>Kartnedlasting</translation>
+    </message>
+    <message>
+        <source>Map settings</source>
+        <translation>Kartinstillinger</translation>
+    </message>
+</context>
+<context>
+    <name>MapDetail</name>
+    <message>
+        <source>Size</source>
+        <translation>Størrelse</translation>
+    </message>
+    <message>
+        <source>Last Update</source>
+        <translation>Sist oppdatering</translation>
+    </message>
+    <message>
+        <source>Download</source>
+        <translation>Last ned</translation>
+    </message>
+    <message>
+        <source>Directory</source>
+        <translation>Katalog</translation>
+    </message>
+</context>
+<context>
+    <name>PlaceDetail</name>
+    <message>
+        <source>west</source>
+        <translation>vest</translation>
+    </message>
+    <message>
+        <source>east</source>
+        <translation>øst</translation>
+    </message>
+    <message>
+        <source>south</source>
+        <translation>sør</translation>
+    </message>
+    <message>
+        <source>north</source>
+        <translation>nord</translation>
+    </message>
+    <message>
+        <source>You are here</source>
+        <translation>Du er her</translation>
+    </message>
+    <message>
+        <source>%1 %2 from you</source>
+        <translation>%1 %2 fra deg</translation>
+    </message>
+    <message>
+        <source>%1 %2 from</source>
+        <translation>%1 %2 fra</translation>
+    </message>
+    <message>
+        <source>meters</source>
+        <translation>meter</translation>
+    </message>
+    <message>
+        <source>km</source>
+        <translation>km</translation>
+    </message>
+</context>
+<context>
+    <name>message</name>
+    <message>
+        <source>Directory already exists</source>
+        <translation>Katolog finnes allerede</translation>
+    </message>
+    <message>
+        <source>Can&apos;t create directory</source>
+        <translation>Kan ikke opprette katalog</translation>
+    </message>
+</context>
+<context>
+    <name>resource</name>
+    <message>
+        <source>© OpenStreetMap contributors</source>
+        <translation>© OpenStreetMap bidragsytere</translation>
+    </message>
+    <message>
+        <source>Hike &amp; Bike Map</source>
+        <translation>Gang- og sykkelkart</translation>
+    </message>
+    <message>
+        <source>OSM Mapnik</source>
+        <translation>OSM Mapnik</translation>
+    </message>
+</context>
+</TS>
