a7a7bf08693655c21c5f93885f98a61169776ca1
PawelSpoon <pawel@ich-habe-fertig.com>
2017-01-01 07:05:45 +0100

    Create de_DE.ts
    german translation

---
+++ translations/de_DE.ts
@@ -0,0 +1,252 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE" sourcelanguage="en_US">
+<context>
+    <name>About</name>
+    <message>
+        <source>OSM Scout %1</source>
+        <translation>OSM Scout %1</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Über OSM Scout</translation>
+    </message>
+    <message>
+        <source>OSM Scout for Sailfish OS is developed as open-source by volunteers in their free time. You can help to improve this application by reporting bugs, creating translations or developing new features. Any help is welcome.</source>
+        <translation>OSM Scout für Sailfish OS wird als Open-Source von Freiwilligen in ihrer Freizeit entwickelt. Sie können helfen diese App zu verbessern, in dem Sie Fehler melden, diese App in eine neue Sprache übersetzen oder neue Funktionen entwickeln. Jede Hilfe ist willkommen.</translation>
+    </message>
+    <message>
+        <source>GitHub page</source>
+        <translation>GitHub Seite</translation>
+    </message>
+    <message>
+        <source>OSMScout library GitHub page</source>
+        <translation>OSMScout library GitHub Seite</translation>
+    </message>
+    <message>
+        <source>Offline map import tutorial</source>
+        <translation>Tutorial importu offline mapy</translation>
+    </message>
+    <message>
+        <source>Translators</source>
+        <translation>Übersetzer</translation>
+    </message>
+    <message>
+        <source>Czech: Lukáš Karas</source>
+        <translation>Tschechisch: Lukáš Karas</translation>
+    </message>
+    <message>
+        <source>Hungarian: Miklós Márton</source>
+        <translation>Ungarisch: Miklós Márton</translation>
+    </message>
+    <message>
+        <source>Polish: Atlochowski</source>
+        <translation>Polnisch: Atlochowski</translation>
+    </message>
+    <message>
+        <source>Swedish: Åke Engelbrektson</source>
+        <translation>Schwedisch: Åke Engelbrektson</translation>
+    </message>
+    <message>
+        <source>Norwegian Bokmål: Håvard Moen</source>
+        <translation>Norwegisch: Håvard Moen</translation>
+    </message>
+</context>
+<context>
+    <name>Cover</name>
+    <message>
+        <source>OSM Scout</source>
+        <translation>OSM Scout</translation>
+    </message>
+</context>
+<context>
+    <name>Downloads</name>
+    <message>
+        <source>Download Progress</source>
+        <translation>Průběh stahování</translation>
+    </message>
+    <message>
+        <source>Download Map</source>
+        <translation>Kartendownloads</translation>
+    </message>
+</context>
+<context>
+    <name>Layers</name>
+    <message>
+        <source>Map settings</source>
+        <translation>Karteneinstellungen</translation>
+    </message>
+    <message>
+        <source>Map magnification</source>
+        <translation>Vergrösserung</translation>
+    </message>
+    <message>
+        <source>Online Maps</source>
+        <translation>Online-Karten</translation>
+    </message>
+    <message>
+        <source>Enable online maps</source>
+        <translation>Online-Karten aktivieren</translation>
+    </message>
+    <message>
+        <source>Style</source>
+        <translation>Stil</translation>
+    </message>
+    <message>
+        <source>Offline Maps</source>
+        <translation>Offline Karten</translation>
+    </message>
+    <message>
+        <source>Enable offline map</source>
+        <translation>Offline Karten erlauben</translation>
+    </message>
+    <message>
+        <source>Sea rendering</source>
+        <translation>Zobrazit moře</translation>
+    </message>
+    <message>
+        <source>Sea can overlap online tiles!</source>
+        <translation>Moře může překrýt online mapu!</translation>
+    </message>
+</context>
+<context>
+    <name>Map</name>
+    <message>
+        <source>Search</source>
+        <translation>Suchen</translation>
+    </message>
+    <message>
+        <source>Where am I?</source>
+        <translation>Wo bin ich?</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation type="vanished">Einstellungen</translation>
+    </message>
+    <message>
+        <source>Layers</source>
+        <translation type="vanished">Vrstvy</translation>
+    </message>
+    <message>
+        <source>Bookmarks</source>
+        <translation>Oblíbená místa</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Über OSM Scout</translation>
+    </message>
+    <message>
+        <source>Map settings</source>
+        <translation>Karten Einstellungen</translation>
+    </message>
+    <message>
+        <source>Map downloads</source>
+        <translation>Stahování map</translation>
+    </message>
+</context>
+<context>
+    <name>MapDetail</name>
+    <message>
+        <source>Size</source>
+        <translation>Größe</translation>
+    </message>
+    <message>
+        <source>Last Update</source>
+        <translation>Letzte Aktualisierung</translation>
+    </message>
+    <message>
+        <source>Download</source>
+        <translation>Stáhnout</translation>
+    </message>
+    <message>
+        <source>Directory</source>
+        <translation>Složka</translation>
+    </message>
+</context>
+<context>
+    <name>OSMCopyright</name>
+    <message>
+        <source>© OpenStreetMap contributors</source>
+        <translation type="vanished">© přispěvatelé OpenStreetMap</translation>
+    </message>
+</context>
+<context>
+    <name>PlaceDetail</name>
+    <message>
+        <source>west</source>
+        <translation>westlich</translation>
+    </message>
+    <message>
+        <source>east</source>
+        <translation>östlich</translation>
+    </message>
+    <message>
+        <source>south</source>
+        <translation>südlich</translation>
+    </message>
+    <message>
+        <source>north</source>
+        <translation>nördlich</translation>
+    </message>
+    <message>
+        <source>You are here</source>
+        <translation>Sie befinden sich hier</translation>
+    </message>
+    <message>
+        <source>%1 %2 from you</source>
+        <translation>%1 %2 von Ihnen</translation>
+    </message>
+    <message>
+        <source>%1 %2 from</source>
+        <translation>%1 %2 von</translation>
+    </message>
+    <message>
+        <source>meters</source>
+        <translation>Meter</translation>
+    </message>
+    <message>
+        <source>km</source>
+        <translation>km</translation>
+    </message>
+</context>
+<context>
+    <name>Search</name>
+    <message>
+        <source>Search places...</source>
+        <translation type="vanished">Plätze suchen...</translation>
+    </message>
+</context>
+<context>
+    <name>l10n</name>
+    <message>
+        <source>© OpenStreetMap contributors</source>
+        <translation type="vanished">© přispěvatelé OpenStreetMap</translation>
+    </message>
+</context>
+<context>
+    <name>message</name>
+    <message>
+        <source>Directory already exists</source>
+        <translation>Ordner existiert bereits</translation>
+    </message>
+    <message>
+        <source>Can&apos;t create directory</source>
+        <translation>Nemohu vytvořit složku</translation>
+    </message>
+</context>
+<context>
+    <name>resource</name>
+    <message>
+        <source>© OpenStreetMap contributors</source>
+        <translation>© OpenStreetMap contributors</translation>
+    </message>
+    <message>
+        <source>Hike &amp; Bike Map</source>
+        <translation>Hike &amp; Bike Karte</translation>
+    </message>
+    <message>
+        <source>OSM Mapnik</source>
+        <translation>OSM Mapnik</translation>
+    </message>
+</context>
+</TS>
