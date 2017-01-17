9c3089a429a79c1ad9f5a63e91a851e1b37db0dc
CARLOS   GONZALEZ <carlosgonz@protonmail.com>
2017-01-16 19:01:29 -0500

    Update osmscout-sailfish-es.ts

--- translations/osmscout-sailfish-es.ts
+++ translations/osmscout-sailfish-es.ts
@@ -1,1 +1,227 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en">
+<context>
+    <name>About</name>
+    <message>
+        <source>OSM Scout %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>OSM Scout for Sailfish OS is developed as open-source by volunteers in their free time. You can help to improve this application by reporting bugs, creating translations or developing new features. Any help is welcome.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>GitHub page</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>OSMScout library GitHub page</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Offline map import tutorial</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Translators</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Czech: Lukáš Karas</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hungarian: Miklós Márton</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Polish: Atlochowski</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Swedish: Åke Engelbrektson</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Norwegian Bokmål: Håvard Moen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>German: Pawel Spoon</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Cover</name>
+    <message>
+        <source>OSM Scout</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Downloads</name>
+    <message>
+        <source>Download Progress</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Download Map</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Layers</name>
+    <message>
+        <source>Map settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Map magnification</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Online Maps</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Enable online maps</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Style</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Offline Maps</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Enable offline map</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sea rendering</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sea can overlap online tiles!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Map</name>
+    <message>
+        <source>Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Where am I?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Bookmarks</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Map downloads</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Map settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MapDetail</name>
+    <message>
+        <source>Size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Last Update</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Download</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PlaceDetail</name>
+    <message>
+        <source>west</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>east</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>south</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>north</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>You are here</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%1 %2 from you</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%1 %2 from</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>meters</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>km</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>message</name>
+    <message>
+        <source>Directory already exists</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Can&apos;t create directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>resource</name>
+    <message>
+        <source>© OpenStreetMap contributors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hike &amp; Bike Map</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>OSM Mapnik</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 
