46c7b16f75e869cd41e3bfa1bb842ca0a8d1f1d6
Lukáš Karas <lukas.karas@centrum.cz>
2017-01-27 09:14:28 +0100

    use upstream marisa-trie code

--- scripts/download-dependencies.sh
+++ scripts/download-dependencies.sh
@@ -7,5 +7,7 @@ cd dependencies
 
 git clone -b master https://github.com/karry-space-with-my-second-forks/libosmscout.git libosmscout
 
-wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/marisa-trie/marisa-0.2.4.tar.gz
-zcat marisa-0.2.4.tar.gz | tar -xf -
+# wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/marisa-trie/marisa-0.2.4.tar.gz
+# zcat marisa-0.2.4.tar.gz | tar -xf -
+
+git clone -b master https://github.com/s-yata/marisa-trie marisa-trie
