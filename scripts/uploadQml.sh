f3edc5f63103b1807d3dbc46e4642aa70c91b2b2
Lukáš Karas <lukas.karas@centrum.cz>
2017-10-17 07:44:48 +0200

    make possible use different host in uploadQml.sh script

--- scripts/uploadQml.sh
+++ scripts/uploadQml.sh
@@ -2,5 +2,10 @@
 
 cd "`dirname $0`/.."
 
-scp -r ./qml root@jolla:/usr/share/harbour-osmscout/
+DEV_SSH_HOST=jolla
+if [ $# -ge 1 ] ; then
+	export DEV_SSH_HOST=$1
+fi
+
+scp -r ./qml root@$DEV_SSH_HOST:/usr/share/harbour-osmscout/
 
