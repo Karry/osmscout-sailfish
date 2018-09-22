4ea90991f1c2d3dd98232d546e94f10be41cde29
Lukáš Karas <lukas.karas@centrum.cz>
2018-09-22 11:30:19 +0200

    upgrade Sailfish OS SDK to 2.2.1.18

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -20,7 +20,7 @@ export PROJECT_TARGET="harbour-osmscout"
 export MER_SSH_SHARED_SRC="$HOME/SailfishOS/projects"
 ## SDK ROOT
 export SDK_ROOT="$HOME/SailfishOS"
-SDK_VERSION=2.2.0.29
+SDK_VERSION=2.2.1.18
 
 ## build & deploy target
 ## list of configured devices: ~/SailfishOS/vmshare/devices.xml 
