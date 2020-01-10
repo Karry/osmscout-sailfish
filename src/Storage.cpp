348bdb65a4d1ce41776e3e7931ff1adee2a7d9f3
Lukáš Karas <lukas.karas@centrum.cz>
2020-01-10 19:35:31 +0100

    rewrite TrackStatisticsAccumulator initialisation from statistics

--- src/Storage.cpp
+++ src/Storage.cpp
@@ -795,30 +795,25 @@ bool Storage::importWaypoints(const gpx::GpxFile &gpxFile, qint64 collectionId)
   return true;
 }
 
-TrackStatisticsAccumulator::TrackStatisticsAccumulator(const TrackStatistics &statistics)
-{
+TrackStatisticsAccumulator::TrackStatisticsAccumulator(const TrackStatistics &statistics):
   // duration accumulator
-  from=dateTimeToTimestampOpt(statistics.from);
-  to=dateTimeToTimestampOpt(statistics.to);
-
+  from{dateTimeToTimestampOpt(statistics.from)},
+  to{dateTimeToTimestampOpt(statistics.to)},
   // bbox
-  bbox=statistics.bbox;
-
+  bbox{statistics.bbox},
   // distance
-  length=statistics.distance;
-
+  length{statistics.distance},
   // raw distance
-  rawLength=statistics.rawDistance;
-
-  // max speed, moving duration
-  maxSpeedBuf.setMaxSpeed(statistics.maxSpeed);
-  movingDuration=statistics.movingDuration;
-
+  rawLength{statistics.rawDistance},
+  // moving duration
+  movingDuration{statistics.movingDuration},
   // elevation
-  minElevation=statistics.minElevation;
-  maxElevation=statistics.maxElevation;
-  ascent=statistics.ascent.AsMeter();
-  descent=statistics.descent.AsMeter();
+  minElevation{statistics.minElevation},
+  maxElevation{statistics.maxElevation},
+  ascent{statistics.ascent.AsMeter()},
+  descent{statistics.descent.AsMeter()}
+{
+  maxSpeedBuf.setMaxSpeed(statistics.maxSpeed);
 }
 
 void TrackStatisticsAccumulator::update(const osmscout::gpx::TrackPoint &p)
