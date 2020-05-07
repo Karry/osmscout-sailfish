b9fdf9dab87cfe4a4013b1f1b9ae4762e7cf7092
Lukáš Karas <lukas.karas@centrum.cz>
2020-05-07 14:16:11 +0200

    update SDK target version

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -20,7 +20,7 @@ export PROJECT_TARGET="harbour-osmscout"
 export MER_SSH_SHARED_SRC="$HOME/SailfishOS/projects"
 ## SDK ROOT
 export SDK_ROOT="$HOME/SailfishOS"
-SDK_VERSION=3.3.0.14EA
+SDK_VERSION=3.3.0.16
 
 ## build & deploy target
 ## list of configured devices: ~/SailfishOS/vmshare/devices.xml 
