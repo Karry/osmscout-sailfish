8613b3ac36e7acce7cf65e6e53d9aea49fafc5a5
carlosgonz <45316884+GNUuser@users.noreply.github.com>
2020-06-18 15:11:56 -0400

    Update es.ts

--- translations/es.ts
+++ translations/es.ts
@@ -419,7 +419,7 @@
     </message>
     <message>
         <source>Download Map</source>
-        <translation type="vanished">Mapas disponibles</translation>
+        <translation>Mapas disponibles</translation>
     </message>
     <message>
         <source>Refresh</source>
@@ -1002,7 +1002,7 @@
     </message>
     <message>
         <source>Available voices</source>
-        <translation type="unfinished"></translation>
+        <translation>Voces disponibles</translation>
     </message>
 </context>
 <context>
@@ -1402,7 +1402,7 @@
     </message>
     <message>
         <source>Available voices</source>
-        <translation type="unfinished"></translation>
+        <translation>Voces disponibles</translation>
     </message>
 </context>
 <context>
