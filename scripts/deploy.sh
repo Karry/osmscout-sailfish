0c242589a0d18310c799c8015608d8857678a796
Lukáš Karas <lukas.karas@centrum.cz>
2021-03-07 14:19:47 +0100

    update SDK target

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,7 +15,7 @@ export TYPE="$1" # emulator
 ##################################################################
 ## configure SDK
 PATH=$PATH:~/SailfishOS/bin/
-export OS_VERSION=3.4.0.24
+export OS_VERSION=4.0.1.48
 
 # device may be configured in SailfishOS SDK
 if [ $# -ge 2 ] ; then
