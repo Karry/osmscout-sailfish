facbf4f99f32797ac6e3b9b11eada08de2ef8f1d
Lukáš Karas <lukas.karas@centrum.cz>
2021-10-13 08:06:09 +0200

    update target OS version to 4.2.0.21

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,7 +15,7 @@ export TYPE="$1" # emulator
 ##################################################################
 ## configure SDK
 PATH=$PATH:~/SailfishOS/bin/
-export OS_VERSION=4.1.0.24
+export OS_VERSION=${OS_VERSION:-4.2.0.21}
 
 # device may be configured in SailfishOS SDK
 if [ $# -ge 2 ] ; then
