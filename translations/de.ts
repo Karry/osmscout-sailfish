22a641293fc0a6f2d4fbda703aa088511266105b
Lukáš Karas <lukas.karas@centrum.cz>
2021-12-06 22:37:01 +0100

    pull German translation

--- translations/de.ts
+++ translations/de.ts
@@ -815,7 +815,7 @@ collection pull down menu</extracomment>
     </message>
     <message>
         <source>Destinations: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Ziele: %1</translation>
     </message>
 </context>
 <context>
@@ -996,11 +996,11 @@ collection pull down menu</extracomment>
     <name>RoutingStep</name>
     <message>
         <source>Distance</source>
-        <translation type="unfinished"></translation>
+        <translation>Entfernung</translation>
     </message>
     <message>
         <source>Destinations: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Ziele: %1</translation>
     </message>
 </context>
 <context>
@@ -1998,47 +1998,47 @@ collection pull down menu</extracomment>
     <message>
         <source>On unnamed exit</source>
         <extracomment>unnamed motorway exit</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Auf unbenannter Ausfahrt</translation>
     </message>
     <message>
         <source>On exit %1</source>
         <extracomment>motorway exit just with ref</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Auf Ausfahrt %1</translation>
     </message>
     <message>
         <source>On exit &quot;%1&quot;</source>
         <extracomment>motorway exit with name, without ref</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Auf Ausfahrt &quot;%1&quot;</translation>
     </message>
     <message>
         <source>On exit %1 &quot;%2&quot;</source>
         <extracomment>motorway exit with ref (%1) and name (%2)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Auf Ausfahrt %1 &quot;%2&quot;</translation>
     </message>
     <message>
         <source>%1 &lt;strong&gt;Change motorway&lt;/strong&gt; from %2 to %3</source>
         <extracomment>%1 is motorway exit description</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>%1 &lt;strong&gt;Wechseln Sie die Autobahn&lt;/strong&gt;  von %2 auf %3</translation>
     </message>
     <message>
         <source>%1 &lt;strong&gt;Change motorway&lt;/strong&gt;</source>
         <extracomment>%1 is motorway exit description</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>%1&lt;strong&gt;Wechseln Sie die Autobahn&lt;/strong&gt;</translation>
     </message>
     <message>
         <source>%1 &lt;strong&gt;Leave motorway&lt;/strong&gt; %2 into %3</source>
         <extracomment>%1 is motorway exit description</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>%1&lt;strong&gt;Verlassen Sie die Autobahn&lt;/strong&gt; %2 auf %3</translation>
     </message>
     <message>
         <source>%1 &lt;strong&gt;Leave motorway&lt;/strong&gt; %2</source>
         <extracomment>%1 is motorway exit description</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>%1&lt;strong&gt;Verlassen Sie die Autobahn&lt;/strong&gt; %2</translation>
     </message>
     <message>
         <source>%1 &lt;strong&gt;Leave motorway&lt;/strong&gt;</source>
         <extracomment>%1 is motorway exit description</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>%1 &lt;strong&gt;Verlassen Sie die Autobahn&lt;/strong&gt;</translation>
     </message>
 </context>
 <context>
