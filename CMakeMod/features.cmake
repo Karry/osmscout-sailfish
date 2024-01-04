f0e47c9d246873b4c224cd95fd63bd50e198f6db
Lukáš Karas <lukas.karas@centrum.cz>
2024-01-04 09:00:59 +0100

    drop iconv dependency

--- CMakeMod/features.cmake
+++ CMakeMod/features.cmake
@@ -110,30 +110,6 @@ if(NOT TARGET LibXml2::LibXml2)
   )
 endif()
 
-find_package(Iconv QUIET)
-if(TARGET Iconv::Iconv)
-  set(HAVE_ICONV TRUE)
-
-  cmake_push_check_state(RESET)
-  set(CMAKE_REQUIRED_INCLUDES ${ICONV_INCLUDE_DIR})
-  set(CMAKE_REQUIRED_LIBRARIES ${ICONV_LIBRARIES})
-  if(MSVC)
-    set(CMAKE_REQUIRED_FLAGS /we4028 /fp:fast /wd4251 /Oi)
-  endif()
-  check_prototype_definition("iconv"
-          "size_t iconv(iconv_t cd, const char **inbuf, size_t *inbytesleft, char **outbuf, size_t *outbytesleft)"
-          "-1"
-          "iconv.h"
-          ICONV_SECOND_ARGUMENT_IS_CONST)
-  cmake_pop_check_state()
-
-  if(${ICONV_SECOND_ARGUMENT_IS_CONST})
-    set(ICONV_CONST "const")
-  endif()
-else()
-  message(WARNING "No iconv support")
-endif()
-
 # prepare cmake variables for configuration files
 set(OSMSCOUT_HAVE_INT16_T ${HAVE_INT16_T})
 set(OSMSCOUT_HAVE_INT32_T ${HAVE_INT32_T})
